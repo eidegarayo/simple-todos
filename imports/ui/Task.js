48752d702ff676d04c2bc0db5949916440548b59
eidegarayo <eva_mig@yahoo.es>
2018-01-26 11:08:49 +0100

    8 adding user accounts

---
+++ imports/ui/Task.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+import { Tasks } from '../api/tasks'
+
+export default class Task extends Component {
+  toggleChecked = () => {
+    Tasks.update(this.props.task._id, {
+      $set: { checked: !this.props.task.checked }
+    })
+  }
+
+  deleteThisTask = () => {
+    Tasks.remove(this.props.task._id)
+  }
+
+  render () {
+    const taskClassName = this.props.task.checked ? 'checked' : ''
+
+    return (
+      <li className={taskClassName}>
+        <button className='delete' onClick={this.deleteThisTask}>
+            &times;
+        </button>
+          
+        <input
+          type='checkbox'
+          readOnly
+          checked={!!this.props.task.cheched}
+          onClick={this.toggleChecked}
+        />
+
+        <span className='text'>
+          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+        </span>
+      </li>
+    )
+  }
+}
